2737aab4500842ce1e8ad60f751e0584cee4c0dd
Abega1642 <hei.abega.2@gmail.com>
2024-09-13 19:43:09 +0300

    feat: add courses list in Teacher class attributes

--- src/main/java/dev/razafindratelo/trackmyclass/entity/users/Teacher.java
+++ src/main/java/dev/razafindratelo/trackmyclass/entity/users/Teacher.java
@@ -1,12 +1,15 @@
 package dev.razafindratelo.trackmyclass.entity.users;
 
+import dev.razafindratelo.trackmyclass.entity.course.Course;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.List;
 
 @Getter
 @Setter
 public class Teacher extends User {
     private boolean isAssistant;
+    private List<Course> courses;
 
     public Teacher(
             String userRef,
@@ -14,9 +17,11 @@ public class Teacher extends User {
             String firstName,
             String email,
             String phoneNumber,
-            boolean isAssistant
+            boolean isAssistant,
+            List<Course> courses
     ) {
         super(userRef, lastName, firstName, email, phoneNumber);
         this.isAssistant = isAssistant;
+        this.courses = courses;
     }
 }
