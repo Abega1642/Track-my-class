632adb98fce31cd64faa0f0ce95f1c3f4a5f5618
Abega1642 <hei.abega.2@gmail.com>
2024-09-13 17:20:51 +0300

    feat: initialize TrackMyClass spring boot application

---
+++ src/main/java/dev/razafindratelo/trackmyclass/TrackMyClassApplication.java
@@ -0,0 +1,13 @@
+package dev.razafindratelo.trackmyclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMyClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackMyClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/razafindratelo/trackmyclass/TrackMyClassApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.razafindratelo.trackmyclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMyClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
