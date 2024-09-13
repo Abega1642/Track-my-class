34d6dd60220d84ffb76eb59347ea579cbde422d4
Abega1642 <hei.abega.2@gmail.com>
2024-09-13 18:48:33 +0300

    feat: add enums package [+(Group, Level) enums inside]

--- src/main/java/dev/razafindratelo/trackmyclass/entity/enums/Group.java
+++ src/main/java/dev/razafindratelo/trackmyclass/entity/enums/Group.java
@@ -1,4 +1,17 @@
 package dev.razafindratelo.trackmyclass.entity.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Group {
+    J1(Level.L1),
+    J2(Level.L1),
+    H1(Level.L2),
+    H2(Level.L2),
+    G1(Level.L3),;
+
+    final Level level;
+    Group(Level level) {
+        this.level = level;
+    }
 }
--- src/main/java/dev/razafindratelo/trackmyclass/entity/enums/Level.java
+++ src/main/java/dev/razafindratelo/trackmyclass/entity/enums/Level.java
@@ -1,4 +1,7 @@
 package dev.razafindratelo.trackmyclass.entity.enums;
 
 public enum Level {
+    L1,
+    L2,
+    L3
 }
