69e6ee9ed462a84bfebf308373a01cae38f2b1e0
Abega1642 <hei.abega.2@gmail.com>
2024-09-14 23:04:41 +0300

    feat: add mapper package [+ (CourseMapper, StudentMapper, TeacherMapper) classes inside]

--- src/main/java/dev/razafindratelo/trackmyclass/mapper/CourseMapper.java
+++ src/main/java/dev/razafindratelo/trackmyclass/mapper/CourseMapper.java
@@ -1,4 +1,16 @@
 package dev.razafindratelo.trackmyclass.mapper;
 
+import dev.razafindratelo.trackmyclass.entity.course.Course;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CourseMapper {
+    public static Course mapToCourse(ResultSet resultSet) throws SQLException {
+        return new Course(
+                resultSet.getString("crs_ref"),
+                resultSet.getString("course_name"),
+                0
+        );
+    }
 }
--- src/main/java/dev/razafindratelo/trackmyclass/mapper/TeacherMapper.java
+++ src/main/java/dev/razafindratelo/trackmyclass/mapper/TeacherMapper.java
@@ -1,4 +1,21 @@
 package dev.razafindratelo.trackmyclass.mapper;
 
+import dev.razafindratelo.trackmyclass.entity.users.Teacher;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 public class TeacherMapper {
+    public static Teacher mapToTeacher(ResultSet resultSet) throws SQLException {
+        return new Teacher(
+                resultSet.getString("tch_ref"),
+                resultSet.getString("teacher_last_name"),
+                resultSet.getString("teacher_first_name"),
+                resultSet.getString("teacher_email"),
+                resultSet.getString("teacher_phone_number"),
+                resultSet.getBoolean("is_assistant"),
+                List.of()
+        );
+    }
 }
