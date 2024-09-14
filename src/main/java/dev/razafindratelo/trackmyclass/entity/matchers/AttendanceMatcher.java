6ede4452a26baa5cefa28c2af80b86748fd19822
Abega1642 <hei.abega.2@gmail.com>
2024-09-14 22:57:17 +0300

    feat: add entity.matchers package [ + AttendanceMatcher class inside]

--- src/main/java/dev/razafindratelo/trackmyclass/entity/matchers/AttendanceMatcher.java
+++ src/main/java/dev/razafindratelo/trackmyclass/entity/matchers/AttendanceMatcher.java
@@ -1,4 +1,16 @@
 package dev.razafindratelo.trackmyclass.entity.matchers;
 
+import dev.razafindratelo.trackmyclass.entity.attendances.Attendance;
+import dev.razafindratelo.trackmyclass.entity.users.Student;
+import dev.razafindratelo.trackmyclass.entity.users.Teacher;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AttendanceMatcher {
+    private Student student;
+    private Attendance attendance;
 }
