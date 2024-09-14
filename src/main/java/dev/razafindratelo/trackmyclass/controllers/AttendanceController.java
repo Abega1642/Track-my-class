483bc85c111e182d04c9695609daefebde86fea2
Abega1642 <hei.abega.2@gmail.com>
2024-09-14 23:17:27 +0300

    feat: add controller package [+ AttendanceController class inside]

--- src/main/java/dev/razafindratelo/trackmyclass/controllers/AttendanceController.java
+++ src/main/java/dev/razafindratelo/trackmyclass/controllers/AttendanceController.java
@@ -1,4 +1,20 @@
 package dev.razafindratelo.trackmyclass.controllers;
 
+import dev.razafindratelo.trackmyclass.dao.AttendanceDAO;
+import dev.razafindratelo.trackmyclass.dao.repository.DBConnection;
+import dev.razafindratelo.trackmyclass.entity.matchers.AttendanceMatcher;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class AttendanceController {
+    @GetMapping("/test")
+    public ResponseEntity<List<AttendanceMatcher>> test() {
+        var db = new DBConnection();
+        var att = new AttendanceDAO(db);
+        return ResponseEntity.ok(att.getAllAttendance());
+    }
 }
