b36f185711e94e6a4798596d04d43b5c23fde77e
Abega1642 <hei.abega.2@gmail.com>
2024-09-14 14:43:32 +0300

    feat: add dao.repository package [+ DBConnection class]

--- src/main/java/dev/razafindratelo/trackmyclass/dao/repository/DBConnection.java
+++ src/main/java/dev/razafindratelo/trackmyclass/dao/repository/DBConnection.java
@@ -1,4 +1,32 @@
 package dev.razafindratelo.trackmyclass.dao.repository;
 
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
+@Repository
+@Getter
 public class DBConnection {
+    private final String url = "jdbc:postgresql://localhost:5432/track_my_class";
+    private final String user = "postgres";
+    private final String password = "xxxx";
+
+    private Connection connection;
+
+    public DBConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(url, user, password);
+            System.out.println(" +++ Connection to database established successfully +++ ");
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC Driver not found : " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Connection to database failed : " + e.getMessage());
+        }
+    }
+
 }
