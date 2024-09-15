62b8f3f61085386228747e59eb0183b4618ff9a5
Abega1642 <hei.abega.2@gmail.com>
2024-09-15 19:28:37 +0300

    feat: add AttendanceServiceImpl

--- src/main/java/dev/razafindratelo/trackmyclass/services/attendanceServices/AttendanceServiceImpl.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/attendanceServices/AttendanceServiceImpl.java
@@ -1,4 +1,27 @@
 package dev.razafindratelo.trackmyclass.services.attendanceServices;
 
-public class AttendanceServiceImpl {
+import dev.razafindratelo.trackmyclass.dao.AttendanceDAO;
+import dev.razafindratelo.trackmyclass.entity.matchers.AttendanceMatcher;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@Getter
+public class AttendanceServiceImpl implements AttendanceService {
+    private final AttendanceDAO attendanceDAO;
+
+    public AttendanceServiceImpl(AttendanceDAO attendanceDAO) {
+        this.attendanceDAO = attendanceDAO;
+    }
+
+    @Override
+    public List<AttendanceMatcher> findAllAttendances() {
+        return attendanceDAO.getAllAttendance();
+    }
+
+    @Override
+    public List<AttendanceMatcher> findAttendancesByStudentRef(String studentRef) {
+        return attendanceDAO.getAttendanceByStudentId(studentRef);
+    }
 }
