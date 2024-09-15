cffd4b561c7c2950f3fb845224c2083cb506bd4b
Abega1642 <hei.abega.2@gmail.com>
2024-09-15 19:29:03 +0300

    chore: re-organize AttendanceController

--- src/main/java/dev/razafindratelo/trackmyclass/controllers/AttendanceController.java
+++ src/main/java/dev/razafindratelo/trackmyclass/controllers/AttendanceController.java
@@ -1,29 +1,28 @@
 package dev.razafindratelo.trackmyclass.controllers;
-
-import dev.razafindratelo.trackmyclass.dao.AttendanceDAO;
-import dev.razafindratelo.trackmyclass.dao.repository.DBConnection;
 import dev.razafindratelo.trackmyclass.entity.matchers.AttendanceMatcher;
+import dev.razafindratelo.trackmyclass.services.attendanceServices.AttendanceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
 public class AttendanceController {
+    private final AttendanceService attendanceService;
+
+    public AttendanceController(AttendanceService attendanceService) {
+        this.attendanceService = attendanceService;
+    }
+
     @GetMapping("/student/attendances")
     public ResponseEntity<List<AttendanceMatcher>> test() {
-        var db = new DBConnection();
-        var att = new AttendanceDAO(db);
-        return ResponseEntity.ok(att.getAllAttendance());
+        return ResponseEntity.ok(attendanceService.findAllAttendances());
     }
 
     @GetMapping("/student/{std}/attendances")
     public ResponseEntity<List<AttendanceMatcher>> getAttendancesById(
             @PathVariable("std") String std) {
-        var db = new DBConnection();
-        var att = new AttendanceDAO(db);
-        return ResponseEntity.ok(att.getAttendanceByStudentId(std));
+        return ResponseEntity.ok(attendanceService.findAttendancesByStudentRef(std));
     }
 }
