15ca05fd45ae7ea0ae5e52f879e2fe867c485927
Abega1642 <hei.abega.2@gmail.com>
2024-09-15 19:02:52 +0300

    feat: add basic test for the endpoint of getAttendanceById

--- src/main/java/dev/razafindratelo/trackmyclass/controllers/AttendanceController.java
+++ src/main/java/dev/razafindratelo/trackmyclass/controllers/AttendanceController.java
@@ -5,6 +5,7 @@ import dev.razafindratelo.trackmyclass.dao.repository.DBConnection;
 import dev.razafindratelo.trackmyclass.entity.matchers.AttendanceMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +17,13 @@ public class AttendanceController {
         var db = new DBConnection();
         var att = new AttendanceDAO(db);
         return ResponseEntity.ok(att.getAllAttendance());
+    }
+
+    @GetMapping("/student/{std}/attendances")
+    public ResponseEntity<List<AttendanceMatcher>> getAttendancesById(
+            @PathVariable("std") String std) {
+        var db = new DBConnection();
+        var att = new AttendanceDAO(db);
+        return ResponseEntity.ok(att.getAttendanceByStudentId(std));
     }
 }
