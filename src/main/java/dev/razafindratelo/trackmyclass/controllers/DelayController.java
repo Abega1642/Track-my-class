9bf5f9e977cbdaee6d91308ea61ac563984d899b
Abega1642 <hei.abega.2@gmail.com>
2024-09-15 21:26:38 +0300

    feat: initialize DelayController class in controller package

--- src/main/java/dev/razafindratelo/trackmyclass/controllers/DelayController.java
+++ src/main/java/dev/razafindratelo/trackmyclass/controllers/DelayController.java
@@ -1,4 +1,19 @@
 package dev.razafindratelo.trackmyclass.controllers;
 
+import dev.razafindratelo.trackmyclass.dao.DelayDAO;
+import dev.razafindratelo.trackmyclass.dao.repository.DBConnection;
+import dev.razafindratelo.trackmyclass.entity.matchers.DelayMatcher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class DelayController {
+
+    @GetMapping("/test")
+    public List<DelayMatcher> test() {
+        var delayDAO = new DelayDAO(new DBConnection());
+        return delayDAO.getAllDelays();
+    }
 }
