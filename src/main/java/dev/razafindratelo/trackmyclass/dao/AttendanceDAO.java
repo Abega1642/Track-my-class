be51f912d665ab4aec0b3a00e32a746a6b13260c
Abega1642 <hei.abega.2@gmail.com>
2024-09-15 19:02:12 +0300

    feat: add getAttendancesById in AttendaceDAO

--- src/main/java/dev/razafindratelo/trackmyclass/dao/AttendanceDAO.java
+++ src/main/java/dev/razafindratelo/trackmyclass/dao/AttendanceDAO.java
@@ -11,11 +11,9 @@ import dev.razafindratelo.trackmyclass.mapper.StudentMapper;
 import dev.razafindratelo.trackmyclass.mapper.TeacherMapper;
 import lombok.Getter;
 import org.springframework.stereotype.Repository;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -81,6 +79,64 @@ public class AttendanceDAO {
             }
         } catch (SQLException e) {
             System.out.println("Error while retrieving attendances : " + e.getMessage());
+        }
+        return attendances;
+    }
+
+    public List<AttendanceMatcher> getAttendanceByStudentId(String studentId) {
+        List<AttendanceMatcher> attendances = new ArrayList<>();
+        try {
+            PreparedStatement getById = dbConnection.getConnection()
+                    .prepareStatement(
+                            """
+                                    SELECT
+                                        student.std_ref,
+                                        student.last_name student_last_name,
+                                        student.first_name student_first_name,
+                                        student.email student_email,
+                                        student.phone_number student_phone_number,
+                                        student.level_year,
+                                        student.group,
+                                        is_present.commencement,
+                                        course.crs_ref,
+                                        course.name course_name,
+                                        is_present.termination,
+                                        teacher.tch_ref,
+                                        teacher.last_name teacher_last_name,
+                                        teacher.first_name teacher_first_name,
+                                        teacher.is_assistant,
+                                        teacher.email teacher_email,
+                                        teacher.phone_number teacher_phone_number
+                                    FROM student INNER JOIN is_present
+                                        ON student.std_ref = is_present.std_ref
+                                    INNER JOIN teacher
+                                        ON is_present.tch_ref = teacher.tch_ref
+                                    INNER JOIN course
+                                        ON course.crs_ref = is_present.crs_ref
+                                    WHERE student.std_ref = ?
+                                """);
+            getById.setString(1, studentId);
+            getById.execute();
+            ResultSet resultSet = getById.getResultSet();
+            while (resultSet.next()) {
+                Student student = StudentMapper.mapToStudent(resultSet);
+                Teacher teacher = TeacherMapper.mapToTeacher(resultSet);
+                Course course = CourseMapper.mapToCourse(resultSet);
+
+                attendances.add(
+                        new AttendanceMatcher(
+                                student,
+                                new Attendance(
+                                    resultSet.getObject("commencement", LocalDateTime.class),
+                                    resultSet.getObject("termination", LocalDateTime.class),
+                                    teacher,
+                                    course
+                                )
+                        )
+                );
+            }
+        } catch(SQLException e) {
+            System.out.println("Error while retrieving attendances by student ref: " + e.getMessage());
         }
         return attendances;
     }
