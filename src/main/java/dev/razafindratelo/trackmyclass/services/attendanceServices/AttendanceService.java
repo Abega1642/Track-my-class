4b243d35a7ebf35e885ce8cc4165547ee9624c2c
Abega1642 <hei.abega.2@gmail.com>
2024-09-15 19:27:35 +0300

    feat: services package [+ AttendanceService class inside]

---
+++ src/main/java/dev/razafindratelo/trackmyclass/services/attendanceServices/AttendanceService.java
@@ -0,0 +1,13 @@
+package dev.razafindratelo.trackmyclass.services.attendanceServices;
+
+import dev.razafindratelo.trackmyclass.entity.matchers.AttendanceMatcher;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface AttendanceService {
+    List<AttendanceMatcher> findAllAttendances();
+
+    List<AttendanceMatcher> findAttendancesByStudentRef(String studentRef);
+}
