deceb3dbd20193eb078c5244c7d4784019549c49
Abega1642 <hei.abega.2@gmail.com>
2024-09-16 21:10:11 +0300

    feat: add Student controller

--- src/main/java/dev/razafindratelo/trackmyclass/controllers/StudentController.java
+++ src/main/java/dev/razafindratelo/trackmyclass/controllers/StudentController.java
@@ -1,4 +1,20 @@
 package dev.razafindratelo.trackmyclass.controllers;
 
+import dev.razafindratelo.trackmyclass.entity.users.Student;
+import dev.razafindratelo.trackmyclass.services.studentServices.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
 public class StudentController {
+    private StudentService studentService;
+
+    @GetMapping("/student/{std}")
+    public ResponseEntity<Student> test(@PathVariable("std") String std) {
+        return ResponseEntity.ok(studentService.findStudentById(std));
+    }
 }
