25bae0fed37ad080e7f2ba662dbc37ac9e59d2a7
Abega1642 <hei.abega.2@gmail.com>
2024-09-17 00:04:06 +0300

    feat: add missing service package [+ (MissingService, MissingServiceImpl) inside]

--- src/main/java/dev/razafindratelo/trackmyclass/services/missingServices/MissingService.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/missingServices/MissingService.java
@@ -1,4 +1,22 @@
 package dev.razafindratelo.trackmyclass.services.missingServices;
 
+import dev.razafindratelo.trackmyclass.entity.matchers.MissingMatcher;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public interface MissingService {
+    List<MissingMatcher> findAllMissing();
+
+    MissingMatcher findMissingByStudentRef(String studentRef);
+
+    MissingMatcher findStudentMissingByCourseRef(String studentRef, String courseName);
+
+    MissingMatcher findStudentMissingByCourseRefThisMonth(String studentRef, String courseName);
+
+    MissingMatcher findNonJustifiedMissingByStudentRef(String studentRef);
+
+    MissingMatcher findMissingByStudentRefThisMonth(String studentRef);
+
+    MissingMatcher findNonJustifiedMissingByStudentRefThisMonth(String studentRef);
 }
--- src/main/java/dev/razafindratelo/trackmyclass/services/missingServices/MissingServiceImpl.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/missingServices/MissingServiceImpl.java
@@ -1,4 +1,68 @@
 package dev.razafindratelo.trackmyclass.services.missingServices;
 
-public class MissingServiceImpl {
+import dev.razafindratelo.trackmyclass.dao.MissingDAO;
+import dev.razafindratelo.trackmyclass.dao.StudentDAO;
+import dev.razafindratelo.trackmyclass.entity.matchers.DelayMatcher;
+import dev.razafindratelo.trackmyclass.entity.matchers.MissingMatcher;
+import dev.razafindratelo.trackmyclass.entity.users.Student;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class MissingServiceImpl implements MissingService {
+    private final MissingDAO missingDAO;
+    private final StudentDAO studentDAO;
+
+    @Override
+    public List<MissingMatcher> findAllMissing() {
+        List<String> studentRef = studentDAO.getAllStudentRef();
+        List<MissingMatcher> missingList = new ArrayList<>();
+
+        for (String std : studentRef) {
+            Student student = studentDAO.getStudentById(std);
+            missingList.add(missingDAO.getMissingByStudent(student));
+        }
+        return missingList;
+    }
+
+    @Override
+    public MissingMatcher findMissingByStudentRef(String studentRef) {
+        Student student = studentDAO.getStudentById(studentRef);
+        return missingDAO.getMissingByStudent(student);
+    }
+
+    @Override
+    public MissingMatcher findStudentMissingByCourseRef(String studentRef, String courseName) {
+        Student student = studentDAO.getStudentById(studentRef);
+        return missingDAO.getStudentMissingByCourse(student, courseName);
+    }
+
+    @Override
+    public MissingMatcher findStudentMissingByCourseRefThisMonth(String studentRef, String courseName) {
+        Student student = studentDAO.getStudentById(studentRef);
+        return missingDAO.getStudentMissingByCourseThisMonth(student, courseName);
+    }
+
+    @Override
+    public MissingMatcher findNonJustifiedMissingByStudentRef(String studentRef) {
+        Student student = studentDAO.getStudentById(studentRef);
+        return missingDAO.getStudentNonJustifiedMissing(student);
+    }
+
+    @Override
+    public MissingMatcher findMissingByStudentRefThisMonth(String studentRef) {
+        Student student = studentDAO.getStudentById(studentRef);
+        return missingDAO.getStudentMissingOfThisMonth(student);
+    }
+
+    @Override
+    public MissingMatcher findNonJustifiedMissingByStudentRefThisMonth(String studentRef) {
+        Student student = studentDAO.getStudentById(studentRef);
+        return missingDAO.getStudentNonJustifiedMissingThisMonth(student);
+    }
+
 }
