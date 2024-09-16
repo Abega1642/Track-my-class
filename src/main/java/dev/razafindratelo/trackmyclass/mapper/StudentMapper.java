fe44bb71c34565c8a5e83cf67f4e003d748779f8
Abega1642 <hei.abega.2@gmail.com>
2024-09-16 20:56:05 +0300

    feat: add second Student mapper

--- src/main/java/dev/razafindratelo/trackmyclass/mapper/StudentMapper.java
+++ src/main/java/dev/razafindratelo/trackmyclass/mapper/StudentMapper.java
@@ -1,19 +1,31 @@
 package dev.razafindratelo.trackmyclass.mapper;
 
-import dev.razafindratelo.trackmyclass.dto.StudentDTO;
+import dev.razafindratelo.trackmyclass.entity.users.Student;
 import dev.razafindratelo.trackmyclass.entity.users.enums.Group;
 import dev.razafindratelo.trackmyclass.entity.users.enums.Level;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class StudentMapper {
-    public static StudentDTO mapToStudentDTO(ResultSet resultSet) throws SQLException {
-        return new StudentDTO(
+    public static Student mapToStudent(ResultSet resultSet) throws SQLException {
+        return new Student(
                 resultSet.getString("std_ref"),
                 resultSet.getString("student_last_name"),
                 resultSet.getString("student_first_name"),
                 resultSet.getString("student_email"),
                 resultSet.getString("student_phone_number"),
+                Level.valueOf(resultSet.getString("level_year")),
+                Group.valueOf(resultSet.getString("group"))
+        );
+    }
+
+    public static Student mapToStudent2(ResultSet resultSet) throws SQLException {
+        return new Student(
+                resultSet.getString("std_ref"),
+                resultSet.getString("last_name"),
+                resultSet.getString("first_name"),
+                resultSet.getString("email"),
+                resultSet.getString("phone_number"),
                 Level.valueOf(resultSet.getString("level_year")),
                 Group.valueOf(resultSet.getString("group"))
         );
