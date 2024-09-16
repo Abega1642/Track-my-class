955664a209d9a15c0cf3ba2f4274cef7c3f793d3
Abega1642 <hei.abega.2@gmail.com>
2024-09-16 19:31:11 +0300

    chore: change Attendance into list of attendances in AttendanceMatcher

--- src/main/java/dev/razafindratelo/trackmyclass/entity/matchers/AttendanceMatcher.java
+++ src/main/java/dev/razafindratelo/trackmyclass/entity/matchers/AttendanceMatcher.java
@@ -6,10 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AttendanceMatcher {
     private StudentDTO student;
-    private Attendance attendance;
+    private List<Attendance> attendance;
 }
