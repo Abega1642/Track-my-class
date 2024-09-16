5bb278eff8f3f3ddb94c9003dcf715dc7eefa599
Abega1642 <hei.abega.2@gmail.com>
2024-09-16 20:58:04 +0300

    refactor: change StudentDto into Student

--- src/main/java/dev/razafindratelo/trackmyclass/entity/matchers/AttendanceMatcher.java
+++ src/main/java/dev/razafindratelo/trackmyclass/entity/matchers/AttendanceMatcher.java
@@ -1,17 +1,16 @@
 package dev.razafindratelo.trackmyclass.entity.matchers;
 
-import dev.razafindratelo.trackmyclass.dto.StudentDTO;
 import dev.razafindratelo.trackmyclass.entity.attendances.Attendance;
+import dev.razafindratelo.trackmyclass.entity.users.Student;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AttendanceMatcher {
-    private StudentDTO student;
+    private Student student;
     private List<Attendance> attendance;
 }
