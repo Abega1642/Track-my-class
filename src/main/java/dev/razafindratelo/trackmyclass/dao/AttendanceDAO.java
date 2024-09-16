a690e501903edcc5c412a266d08596b15c72a8ce
Abega1642 <hei.abega.2@gmail.com>
2024-09-16 21:08:12 +0300

    chore: re-implement AttendanceDAO according to StudentDAO structure

--- src/main/java/dev/razafindratelo/trackmyclass/dao/AttendanceDAO.java
+++ src/main/java/dev/razafindratelo/trackmyclass/dao/AttendanceDAO.java
@@ -1,13 +1,11 @@
 package dev.razafindratelo.trackmyclass.dao;
 
 import dev.razafindratelo.trackmyclass.dao.repository.DBConnection;
-import dev.razafindratelo.trackmyclass.dto.StudentDTO;
 import dev.razafindratelo.trackmyclass.dto.TeacherDTO;
 import dev.razafindratelo.trackmyclass.entity.attendances.Attendance;
 import dev.razafindratelo.trackmyclass.entity.course.Course;
 import dev.razafindratelo.trackmyclass.entity.matchers.AttendanceMatcher;
 import dev.razafindratelo.trackmyclass.entity.users.Student;
-import dev.razafindratelo.trackmyclass.entity.users.Teacher;
 import dev.razafindratelo.trackmyclass.mapper.CourseMapper;
 import dev.razafindratelo.trackmyclass.mapper.StudentMapper;
 import dev.razafindratelo.trackmyclass.mapper.TeacherMapper;
@@ -31,74 +29,25 @@ public class AttendanceDAO {
 
     public List<AttendanceMatcher> getAllAttendance() {
         List<AttendanceMatcher> attendances = new ArrayList<>();
-        try {
-            PreparedStatement getAll = dbConnection
-                    .getConnection()
-                    .prepareStatement(
-                            """
-                                    SELECT
-                                        student.std_ref,
-                                        student.last_name student_last_name,
-                                        student.first_name student_first_name,
-                                        student.email student_email,
-                                        student.phone_number student_phone_number,
-                                        student.level_year,
-                                        student.group,
-                                        is_present.commencement,
-                                        course.crs_ref,
-                                        course.name course_name,
-                                        is_present.termination,
-                                        teacher.tch_ref,
-                                        teacher.last_name teacher_last_name,
-                                        teacher.first_name teacher_first_name,
-                                        teacher.is_assistant,
-                                        teacher.email teacher_email,
-                                        teacher.phone_number teacher_phone_number
-                                    FROM student INNER JOIN is_present
-                                        ON student.std_ref = is_present.std_ref
-                                    INNER JOIN teacher
-                                        ON is_present.tch_ref = teacher.tch_ref
-                                    INNER JOIN course
-                                        ON course.crs_ref = is_present.crs_ref
-                                """);
-            getAll.execute();
-            ResultSet resultSet = getAll.getResultSet();
-            while (resultSet.next()) {
-                StudentDTO student = StudentMapper.mapToStudentDTO(resultSet);
-                TeacherDTO teacher = TeacherMapper.mapToTeacherDTO(resultSet);
-                Course course = CourseMapper.mapToCourse(resultSet);
-                attendances.add(
-                        new AttendanceMatcher(
-                            student,
-                            new Attendance(
-                                    resultSet.getObject("commencement", LocalDateTime.class),
-                                    resultSet.getObject("termination", LocalDateTime.class),
-                                    teacher,
-                                    course
-                            )
-                        )
-                );
-            }
-        } catch (SQLException e) {
-            System.out.println("Error while retrieving attendances : " + e.getMessage());
+        List<String> stds = getAllStudentRef();
+        for (String std : stds) {
+            Student student = getStudent(std);
+            attendances.add(getAttendanceByStudent(student));
         }
         return attendances;
     }
 
-    public List<AttendanceMatcher> getAttendanceByStudentId(String studentId) {
-        List<AttendanceMatcher> attendances = new ArrayList<>();
+    public AttendanceMatcher getAttendanceByStudent(Student student) {
+
+        List<Attendance> attendances = new ArrayList<>();
+        AttendanceMatcher attendanceMatcher = new AttendanceMatcher(student, List.of());
+
         try {
             PreparedStatement getById = dbConnection.getConnection()
                     .prepareStatement(
                             """
                                     SELECT
-                                        student.std_ref,
-                                        student.last_name student_last_name,
-                                        student.first_name student_first_name,
-                                        student.email student_email,
-                                        student.phone_number student_phone_number,
-                                        student.level_year,
-                                        student.group,
+                                        is_present.std_ref,
                                         is_present.commencement,
                                         course.crs_ref,
                                         course.name course_name,
@@ -109,38 +58,80 @@ public class AttendanceDAO {
                                         teacher.is_assistant,
                                         teacher.email teacher_email,
                                         teacher.phone_number teacher_phone_number
-                                    FROM student INNER JOIN is_present
-                                        ON student.std_ref = is_present.std_ref
-                                    INNER JOIN teacher
+                                    FROM teacher INNER JOIN is_present
                                         ON is_present.tch_ref = teacher.tch_ref
                                     INNER JOIN course
                                         ON course.crs_ref = is_present.crs_ref
-                                    WHERE student.std_ref = ?
+                                    WHERE is_present.std_ref = ?
                                 """);
-            getById.setString(1, studentId);
+
+            getById.setString(1, student.getUserRef());
             getById.execute();
             ResultSet resultSet = getById.getResultSet();
+
             while (resultSet.next()) {
-                StudentDTO student = StudentMapper.mapToStudentDTO(resultSet);
                 TeacherDTO teacher = TeacherMapper.mapToTeacherDTO(resultSet);
                 Course course = CourseMapper.mapToCourse(resultSet);
 
                 attendances.add(
-                        new AttendanceMatcher(
-                                student,
-                                new Attendance(
-                                    resultSet.getObject("commencement", LocalDateTime.class),
-                                    resultSet.getObject("termination", LocalDateTime.class),
-                                    teacher,
-                                    course
-                                )
+                        new Attendance(
+                                resultSet.getObject("commencement", LocalDateTime.class),
+                                resultSet.getObject("termination", LocalDateTime.class),
+                                teacher,
+                                course
                         )
                 );
             }
+
+            attendanceMatcher.setAttendance(attendances);
         } catch(SQLException e) {
             System.out.println("Error while retrieving attendances by student ref: " + e.getMessage());
         }
-        return attendances;
+        return attendanceMatcher;
     }
 
+    public Student getStudent(String std) {
+        Student student = null;
+        try {
+            PreparedStatement getStudent = dbConnection
+                    .getConnection()
+                    .prepareStatement(
+                            """
+                                SELECT * FROM student
+                                WHERE std_ref = ?
+                                """
+                    );
+            getStudent.setString(1, std);
+            ResultSet resultSet = getStudent.executeQuery();
+            if (resultSet.next()) {
+                student = StudentMapper.mapToStudent2(resultSet);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error while retrieving student : " + e.getMessage());
+        }
+        return student;
+    }
+
+    public List<String> getAllStudentRef() {
+        List<String> studentRefs = new ArrayList<>();
+        try {
+            PreparedStatement getAllSTDs = dbConnection
+                    .getConnection()
+                    .prepareStatement(
+                            """
+                                    SELECT std_ref FROM student
+                                """
+                    );
+            getAllSTDs.execute();
+            ResultSet resultSet = getAllSTDs.getResultSet();
+            while (resultSet.next()) {
+                studentRefs.add(resultSet.getString("std_ref"));
+            }
+
+        } catch(SQLException e) {
+            System.out.println("Error while retrieving students ref: " + e.getMessage());
+        }
+        return studentRefs;
+    }
 }
