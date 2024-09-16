4ca0a0deb8f4f2f08d067ab45282f25800640bb1
Abega1642 <hei.abega.2@gmail.com>
2024-09-16 21:29:04 +0300

    chore: transfert all student method from attendance dao in student dao

--- src/main/java/dev/razafindratelo/trackmyclass/dao/StudentDAO.java
+++ src/main/java/dev/razafindratelo/trackmyclass/dao/StudentDAO.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Repository;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @Getter
@@ -38,5 +40,50 @@ public class StudentDAO {
             System.out.println("Error while finding student by id: " + studentId + " : " + e.getMessage());
         }
         return student;
+    }
+
+    public Student getStudent(String std) {
+        Student student = null;
+        try {
+            PreparedStatement getStudent = dbConnection
+                    .getConnection()
+                    .prepareStatement(
+                            """
+                                SELECT * FROM student
+                                WHERE std_ref = ?
+                                """
+                    );
+            getStudent.setString(1, std);
+            ResultSet resultSet = getStudent.executeQuery();
+            if (resultSet.next()) {
+                student = StudentMapper.mapToStudent2(resultSet);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error while retrieving student : " + e.getMessage());
+        }
+        return student;
+    }
+
+    public List<String> getAllStudentRef() {
+        List<String> studentRefs = new ArrayList<>();
+        try {
+            PreparedStatement getAllSTDs = dbConnection
+                    .getConnection()
+                    .prepareStatement(
+                            """
+                                    SELECT std_ref FROM student
+                                """
+                    );
+            getAllSTDs.execute();
+            ResultSet resultSet = getAllSTDs.getResultSet();
+            while (resultSet.next()) {
+                studentRefs.add(resultSet.getString("std_ref"));
+            }
+
+        } catch(SQLException e) {
+            System.out.println("Error while retrieving students ref: " + e.getMessage());
+        }
+        return studentRefs;
     }
 }
