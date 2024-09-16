47398b620a8c5e824329c48e011422662312624c
Abega1642 <hei.abega.2@gmail.com>
2024-09-16 20:54:23 +0300

    feat: add studentServices package [+ (StudentService, StudentServiceImpl) inseide]

--- src/main/java/dev/razafindratelo/trackmyclass/services/studentServices/StudentService.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/studentServices/StudentService.java
@@ -1,4 +1,9 @@
 package dev.razafindratelo.trackmyclass.services.studentServices;
 
+import dev.razafindratelo.trackmyclass.entity.users.Student;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface StudentService {
+    Student findStudentById(String std);
 }
--- src/main/java/dev/razafindratelo/trackmyclass/services/studentServices/StudentServiceImpl.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/studentServices/StudentServiceImpl.java
@@ -1,4 +1,19 @@
 package dev.razafindratelo.trackmyclass.services.studentServices;
 
-public class StudentServiceImpl {
+import dev.razafindratelo.trackmyclass.dao.StudentDAO;
+import dev.razafindratelo.trackmyclass.entity.users.Student;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Getter
+public class StudentServiceImpl implements StudentService {
+    private StudentDAO studentDAO;
+
+    @Override
+    public Student findStudentById(String std) {
+        return studentDAO.getStudentById(std);
+    }
 }
