056b1404f9c5ab6cb88f359517384120c36e1483
Abega1642 <hei.abega.2@gmail.com>
2024-09-20 10:35:59 +0300

    feat: add courseServices package [+ (CourseService, CourseServiceImpl) inseide]

--- src/main/java/dev/razafindratelo/trackmyclass/services/courseServices/CourseService.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/courseServices/CourseService.java
@@ -1,4 +1,11 @@
 package dev.razafindratelo.trackmyclass.services.courseServices;
 
+import dev.razafindratelo.trackmyclass.entity.course.Course;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface CourseService {
+    Course getCourseById(String courseId);
+
+    Course getCourseByName(String name);
 }
--- src/main/java/dev/razafindratelo/trackmyclass/services/courseServices/CourseServiceImpl.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/courseServices/CourseServiceImpl.java
@@ -1,4 +1,31 @@
 package dev.razafindratelo.trackmyclass.services.courseServices;
 
-public class CourseServiceImpl {
+import dev.razafindratelo.trackmyclass.dao.CourseDAO;
+import dev.razafindratelo.trackmyclass.entity.course.Course;
+import dev.razafindratelo.trackmyclass.exceptionHandler.IllegalRequestException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CourseServiceImpl implements CourseService {
+    private final CourseDAO courseDAO;
+
+    @Override
+    public Course getCourseById(String courseId) {
+        if(courseId == null || courseId.isEmpty()) {
+            throw new IllegalRequestException("Course ID cannot be null or empty");
+        } else {
+            return courseDAO.getCourseByRef(courseId);
+        }
+    }
+
+    @Override
+    public Course getCourseByName(String name) {
+        if(name == null || name.isEmpty()) {
+            throw new IllegalRequestException("Course name cannot be null or empty");
+        } else {
+            return courseDAO.getCourseByName(name);
+        }
+    }
 }
