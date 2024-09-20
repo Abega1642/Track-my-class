864334b76132f73355429a0548bb8fbd4661de7b
Abega1642 <hei.abega.2@gmail.com>
2024-09-20 15:57:51 +0300

    chore: remove unused mapper method in StudentMapper

--- src/main/java/dev/razafindratelo/trackmyclass/mapper/StudentMapper.java
+++ src/main/java/dev/razafindratelo/trackmyclass/mapper/StudentMapper.java
@@ -7,19 +7,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class StudentMapper {
-    public static Student mapToStudent(ResultSet resultSet) throws SQLException {
-        return new Student(
-                resultSet.getString("std_ref"),
-                resultSet.getString("student_last_name"),
-                resultSet.getString("student_first_name"),
-                resultSet.getString("student_email"),
-                resultSet.getString("student_phone_number"),
-                Level.valueOf(resultSet.getString("level_year")),
-                Group.valueOf(resultSet.getString("group"))
-        );
-    }
 
-    public static Student mapToStudent2(ResultSet resultSet) throws SQLException {
+    public static Student mapToStudent(ResultSet resultSet) throws SQLException {
         return new Student(
                 resultSet.getString("std_ref"),
                 resultSet.getString("last_name"),
