a498b4fdcd49262cf8173002eb034ea91df13edf
Abega1642 <hei.abega.2@gmail.com>
2024-09-20 15:55:57 +0300

    feat: implement update (PUT, PATCH) method in TeacherController

--- src/main/java/dev/razafindratelo/trackmyclass/controllers/StudentController.java
+++ src/main/java/dev/razafindratelo/trackmyclass/controllers/StudentController.java
@@ -8,22 +8,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RestController("/student")
+@RestController
 @AllArgsConstructor
 public class StudentController {
     private StudentService studentService;
 
-    @GetMapping("/list")
+    @GetMapping("/student/list")
     public ResponseEntity<List<Student>> getAllStudent() {
         return ResponseEntity.ok(studentService.findAllStudents());
     }
 
-    @GetMapping("/{std}")
+    @GetMapping("/student/{std}")
     public ResponseEntity<Student> getStudentInfo(@PathVariable("std") String std) {
         return ResponseEntity.ok(studentService.findStudentById(std));
     }
 
-    @PostMapping("/add")
+    @PostMapping("/student/add")
     public ResponseEntity<Student> addStudent(@RequestBody Student student) {
         Student addedStudent = studentService.insertStudent(student);
         return new ResponseEntity<>(addedStudent, HttpStatus.CREATED);
--- src/main/java/dev/razafindratelo/trackmyclass/controllers/TeacherController.java
+++ src/main/java/dev/razafindratelo/trackmyclass/controllers/TeacherController.java
@@ -1,4 +1,42 @@
 package dev.razafindratelo.trackmyclass.controllers;
 
+import dev.razafindratelo.trackmyclass.entity.users.Teacher;
+import dev.razafindratelo.trackmyclass.services.teacherServices.TeacherService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
 public class TeacherController {
+    private TeacherService teacherService;
+
+    @GetMapping("/teachers")
+    public ResponseEntity<List<Teacher>> getAllTeachers() {
+        return ResponseEntity.ok(teacherService.findAllTeachers());
+    }
+
+    @GetMapping("/teacher/{teacherRef}")
+    public ResponseEntity<Teacher> getTeacherById(@PathVariable("teacherRef") String teacherRef) {
+        Teacher teacher = teacherService.findTeacherById(teacherRef);
+        return ResponseEntity.ok(teacher);
+    }
+
+    @PutMapping("/teacher/{teacherRef}/update")
+    public ResponseEntity<Teacher> updateTeacherById(
+            @PathVariable("teacherRef") String teacherRef,
+            @RequestBody Teacher teacher) {
+        return ResponseEntity.ok(teacherService.updateTeacher(teacherRef, teacher));
+    }
+
+    @PatchMapping("/teacher/{teacherRef}/update")
+    public ResponseEntity<Teacher> updateTeacherPartiallyById(
+            @PathVariable("teacherRef") String teacherRef,
+            @RequestBody Teacher teacher
+    ) throws NoSuchFieldException, IllegalAccessException {
+        return ResponseEntity.ok(teacherService.partialTeacherUpdate(teacherRef,teacher));
+    }
+
 }
