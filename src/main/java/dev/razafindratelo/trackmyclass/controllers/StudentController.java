ab295ac2e3adc67cf4a7774fc03e2d7391ca899e
Abega1642 <hei.abega.2@gmail.com>
2024-09-20 10:43:07 +0300

    chore: re-implement StudentController enpoint structures

--- src/main/java/dev/razafindratelo/trackmyclass/controllers/StudentController.java
+++ src/main/java/dev/razafindratelo/trackmyclass/controllers/StudentController.java
@@ -6,25 +6,24 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
-@RestController
+@RestController("/student")
 @AllArgsConstructor
 public class StudentController {
     private StudentService studentService;
 
-    @GetMapping("/student/list")
+    @GetMapping("/list")
     public ResponseEntity<List<Student>> getAllStudent() {
         return ResponseEntity.ok(studentService.findAllStudents());
     }
 
-    @GetMapping("/student/{std}")
+    @GetMapping("/{std}")
     public ResponseEntity<Student> getStudentInfo(@PathVariable("std") String std) {
         return ResponseEntity.ok(studentService.findStudentById(std));
     }
 
-    @PostMapping("/student/add")
+    @PostMapping("/add")
     public ResponseEntity<Student> addStudent(@RequestBody Student student) {
         Student addedStudent = studentService.insertStudent(student);
         return new ResponseEntity<>(addedStudent, HttpStatus.CREATED);
