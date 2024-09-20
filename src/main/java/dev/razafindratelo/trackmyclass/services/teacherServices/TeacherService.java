3dc61294057f63b903f20d572eafe5beb4758247
Abega1642 <hei.abega.2@gmail.com>
2024-09-20 10:35:11 +0300

    feat: add teacherServices package [+ (TeacherService, TeacherServiceImpl) inside]

--- src/main/java/dev/razafindratelo/trackmyclass/services/teacherServices/TeacherService.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/teacherServices/TeacherService.java
@@ -1,4 +1,9 @@
 package dev.razafindratelo.trackmyclass.services.teacherServices;
 
+import dev.razafindratelo.trackmyclass.entity.users.Teacher;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface TeacherService {
+    Teacher findTeacherById(String teacherRef);
 }
--- src/main/java/dev/razafindratelo/trackmyclass/services/teacherServices/TeacherServiceImpl.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/teacherServices/TeacherServiceImpl.java
@@ -1,4 +1,22 @@
 package dev.razafindratelo.trackmyclass.services.teacherServices;
 
-public class TeacherServiceImpl {
+import dev.razafindratelo.trackmyclass.dao.TeacherDAO;
+import dev.razafindratelo.trackmyclass.entity.users.Teacher;
+import dev.razafindratelo.trackmyclass.exceptionHandler.IllegalRequestException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TeacherServiceImpl implements TeacherService {
+    private TeacherDAO teacherDAO;
+
+    @Override
+    public Teacher findTeacherById(String teacherRef) {
+        if (teacherRef == null || teacherRef.isEmpty()) {
+            throw new IllegalRequestException("Teacher ref must not be null or empty");
+        } else {
+            return this.teacherDAO.findTeacherById(teacherRef);
+        }
+    }
 }
