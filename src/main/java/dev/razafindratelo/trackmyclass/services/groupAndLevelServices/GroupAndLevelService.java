0cd8b7346c3901fdb9879ddab558e64e7605d65d
Abega1642 <hei.abega.2@gmail.com>
2024-09-22 23:49:16 +0300

    feat: implement more methods in groupAndLevelServices

--- src/main/java/dev/razafindratelo/trackmyclass/services/groupAndLevelServices/GroupAndLevelService.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/groupAndLevelServices/GroupAndLevelService.java
@@ -1,10 +1,24 @@
 package dev.razafindratelo.trackmyclass.services.groupAndLevelServices;
 
+import dev.razafindratelo.trackmyclass.entity.matchers.LevelGroupMatcher;
+import dev.razafindratelo.trackmyclass.entity.users.Student;
 import dev.razafindratelo.trackmyclass.entity.users.enums.Group;
 import dev.razafindratelo.trackmyclass.entity.users.enums.Level;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public interface GroupAndLevelService {
+    List<LevelGroupMatcher> getAllLevelAndGroupRelations();
+
+    List<Group> getAllGroups();
+
+    List<Level> getAllLevels();
+
     boolean checkLevelAndGroupMap(Group group, Level level);
+
+    List<LevelGroupMatcher> updateGroupAndLevelRelation();
+
+    List<Student> updateAllLevelGroupRelationAndStudentToNextLevelYear(List<String> excludeSTDs);
+
 }
--- src/main/java/dev/razafindratelo/trackmyclass/services/groupAndLevelServices/GroupAndLevelServiceImpl.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/groupAndLevelServices/GroupAndLevelServiceImpl.java
@@ -1,21 +1,54 @@
 package dev.razafindratelo.trackmyclass.services.groupAndLevelServices;
 
 import dev.razafindratelo.trackmyclass.dao.GroupAndLevelDAO;
+import dev.razafindratelo.trackmyclass.entity.matchers.LevelGroupMatcher;
+import dev.razafindratelo.trackmyclass.entity.users.Student;
 import dev.razafindratelo.trackmyclass.entity.users.enums.Group;
 import dev.razafindratelo.trackmyclass.entity.users.enums.Level;
+import dev.razafindratelo.trackmyclass.services.studentServices.StudentService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Data
 @Service
 @AllArgsConstructor
 public class GroupAndLevelServiceImpl implements GroupAndLevelService {
     private final GroupAndLevelDAO groupAndLevelDAO;
+    private final StudentService studentService;
+
+
+    @Override
+    public List<LevelGroupMatcher> getAllLevelAndGroupRelations() {
+        return groupAndLevelDAO.getAllRelationOfLeveAndGroup();
+    }
+
+    @Override
+    public List<Group> getAllGroups() {
+        return groupAndLevelDAO.allGroups();
+    }
+
+    @Override
+    public List<Level> getAllLevels() {
+        return groupAndLevelDAO.allLevels();
+    }
 
     @Override
     public boolean checkLevelAndGroupMap(Group group, Level level) {
         return groupAndLevelDAO.doesLevelMatchGroup(group, level);
+    }
+
+    @Override
+    public List<LevelGroupMatcher> updateGroupAndLevelRelation() {
+        return groupAndLevelDAO.updateGroupLevels();
+    }
+
+    @Override
+    public List<Student> updateAllLevelGroupRelationAndStudentToNextLevelYear(List<String> excludeSTDs) {
+        List<LevelGroupMatcher> updatedGroupAndLevelRelations = updateGroupAndLevelRelation();
+        return studentService.updateLevels(updatedGroupAndLevelRelations, excludeSTDs);
     }
 
 
