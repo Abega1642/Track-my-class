2bafe1794300a56b69117252c7b8b4192a8d04d0
Abega1642 <hei.abega.2@gmail.com>
2024-09-22 17:47:06 +0300

    refactor: change method signature according to DTOs structures

--- src/main/java/dev/razafindratelo/trackmyclass/services/attendanceServices/AttendanceService.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/attendanceServices/AttendanceService.java
@@ -1,8 +1,9 @@
 package dev.razafindratelo.trackmyclass.services.attendanceServices;
 
 import dev.razafindratelo.trackmyclass.dto.AttendanceDTO;
+import dev.razafindratelo.trackmyclass.dto.GeneralAttendanceDTO;
 import dev.razafindratelo.trackmyclass.dto.GeneralMissingDTO;
-import dev.razafindratelo.trackmyclass.dto.GroupMissingDTO;
+import dev.razafindratelo.trackmyclass.dto.GroupAttendanceDTO;
 import dev.razafindratelo.trackmyclass.entity.attendances.Attendance;
 import dev.razafindratelo.trackmyclass.entity.matchers.AttendanceMatcher;
 import dev.razafindratelo.trackmyclass.entity.matchers.GenericAttendanceMatcher;
@@ -19,13 +20,12 @@ public interface AttendanceService {
 
     List<AttendanceMatcher> addStudentsAttendance(AttendanceDTO attendances);
 
-    List<GenericAttendanceMatcher<?>> doAttendanceByLevelYear(GeneralMissingDTO missing);
+    List<GenericAttendanceMatcher<?>> doAttendanceByLevelYear(GeneralAttendanceDTO missing);
 
-    List<GenericAttendanceMatcher<?>> doAttendanceByGroup(GroupMissingDTO missing);
+    List<GenericAttendanceMatcher<?>> doAttendanceByGroup(GroupAttendanceDTO missing);
 
     List<AttendanceMatcher> addStudentsAttendances(
             List<String> presentSTDs,
-            GeneralMissingDTO missing,
-            String teacherRef
+            GeneralAttendanceDTO missing
     );
 }
--- src/main/java/dev/razafindratelo/trackmyclass/services/attendanceServices/AttendanceServiceImpl.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/attendanceServices/AttendanceServiceImpl.java
@@ -1,17 +1,17 @@
 package dev.razafindratelo.trackmyclass.services.attendanceServices;
 
 import dev.razafindratelo.trackmyclass.dao.AttendanceDAO;
-import dev.razafindratelo.trackmyclass.dto.AttendanceDTO;
-import dev.razafindratelo.trackmyclass.dto.GeneralMissingDTO;
-import dev.razafindratelo.trackmyclass.dto.GroupMissingDTO;
+import dev.razafindratelo.trackmyclass.dto.*;
 import dev.razafindratelo.trackmyclass.entity.attendances.Attendance;
 import dev.razafindratelo.trackmyclass.entity.course.Course;
 import dev.razafindratelo.trackmyclass.entity.matchers.AttendanceMatcher;
+import dev.razafindratelo.trackmyclass.entity.matchers.DelayMatcher;
 import dev.razafindratelo.trackmyclass.entity.matchers.GenericAttendanceMatcher;
 import dev.razafindratelo.trackmyclass.entity.matchers.MissingMatcher;
 import dev.razafindratelo.trackmyclass.entity.users.Student;
 import dev.razafindratelo.trackmyclass.entity.users.Teacher;
 import dev.razafindratelo.trackmyclass.services.courseServices.CourseService;
+import dev.razafindratelo.trackmyclass.services.delayServices.DelayService;
 import dev.razafindratelo.trackmyclass.services.missingServices.MissingService;
 import dev.razafindratelo.trackmyclass.services.studentServices.StudentService;
 import dev.razafindratelo.trackmyclass.services.teacherServices.TeacherService;
@@ -30,6 +30,7 @@ public class AttendanceServiceImpl implements AttendanceService {
     private final TeacherService teacherService;
     private final CourseService courseService;
     private final MissingService missingService;
+    private final DelayService delayService;
 
 
     @Override
@@ -46,7 +47,7 @@ public class AttendanceServiceImpl implements AttendanceService {
 
     @Override
     public AttendanceMatcher findAttendancesByStudentRef(String studentRef) {
-        Student student = studentService.findStudentById(studentRef);
+        Student student = studentService.findStudentById(studentRef.toUpperCase());
         return attendanceDAO.getAttendanceByStudent(student);
     }
 
@@ -91,82 +92,117 @@ public class AttendanceServiceImpl implements AttendanceService {
     }
 
     @Override
-    public List<GenericAttendanceMatcher<?>> doAttendanceByLevelYear(GeneralMissingDTO missing) {
+    public List<GenericAttendanceMatcher<?>> doAttendanceByLevelYear(GeneralAttendanceDTO missing) {
         String levelYear = courseService.getCourseLevelYear(missing.getCourseName()).toString().toUpperCase();
-        String teacherRef = missing.getResponsibleRef().toUpperCase();
 
         // list of justified missing STDs
         List<String> justifiedMissingSTDs = studentService
                 .filterExistingStudentByLevelYear(missing.getStdsWithMissingJustification(), levelYear);
+        missing.setStdsWithMissingJustification(justifiedMissingSTDs);
 
         // list of unjustified missing STDs
         List<String> unjustifiedMissingSTDs = studentService
                 .filterExistingStudentByLevelYear(missing.getStdsWithoutMissingJustification(), levelYear);
 
+        missing.setStdsWithoutMissingJustification(unjustifiedMissingSTDs);
+
         // list of all missing STDS (whether justified or not)
         List<String> allMissingSTDs = new ArrayList<>();
         allMissingSTDs.addAll(justifiedMissingSTDs);
         allMissingSTDs.addAll(unjustifiedMissingSTDs);
 
+        // list of all delay STDs with their respective lateness
+        List<String> delayedSTDs = missing.getStudentDelays().stream().map(StudentDelayDTO::getStd).toList();
+
+        List<String> filteredDelayedSTDs = studentService
+                .filterExistingStudentByLevelYear(delayedSTDs, levelYear);
+
+        List<StudentDelayDTO> delays = missing.getStudentDelays()
+                .stream()
+                .filter(d -> filteredDelayedSTDs.contains(d.getStd()))
+                .toList();
+        missing.setStudentDelays(delays);
+
         // list of all student STDs which were present at the course
         List<String> presentSTDs = studentService.filterPresentStdsByLevelYear(allMissingSTDs, levelYear);
 
-        // list of all attendances (missing & presence)  of all stds
-        List<AttendanceMatcher> attendanceMatchers = addStudentsAttendances(presentSTDs, missing, teacherRef);
+        // list of all attendances (missing & presence & delays)  of all stds
+        List<AttendanceMatcher> attendanceMatchers = addStudentsAttendances(presentSTDs, missing);
         List<MissingMatcher> missingMatchers = missingService
                 .completeMissing(missing);
+        List<DelayMatcher> delayMatchers = delayService.completeAddDelay(delays, missing);
 
         // result
-        return completeAttendances(attendanceMatchers, missingMatchers);
+        return completeAttendances(attendanceMatchers, missingMatchers, delayMatchers);
     }
 
     @Override
-    public List<GenericAttendanceMatcher<?>> doAttendanceByGroup(GroupMissingDTO missing) {
+    public List<GenericAttendanceMatcher<?>> doAttendanceByGroup(GroupAttendanceDTO missing) {
         String group = missing.getGroup().toString().toUpperCase();
-        String teacherRef = missing.getResponsibleRef().toUpperCase();
-
-        // list of justified missing STDs
-        List<String> justifiedMissingSTDs = studentService
-                .filterExistingStudentByGroup(missing.getStdsWithMissingJustification(), group);
 
         // list of unjustified missing STDs
         List<String> unjustifiedMissingSTDs = studentService
                 .filterExistingStudentByGroup(missing.getStdsWithoutMissingJustification(), group);
 
+        missing.setStdsWithoutMissingJustification(unjustifiedMissingSTDs);
+
+        // list of justified missing STDs
+        List<String> justifiedMissingSTDs = studentService
+                .filterExistingStudentByGroup(missing.getStdsWithMissingJustification(), group);
+
+        missing.setStdsWithMissingJustification(justifiedMissingSTDs);
+
         // list of all missing STDS (whether justified or not)
         List<String> allMissingSTDs = new ArrayList<>();
         allMissingSTDs.addAll(justifiedMissingSTDs);
         allMissingSTDs.addAll(unjustifiedMissingSTDs);
 
+        // list of all delay STDs with their respective lateness
+        List<String> delayedSTDs = missing.getStudentDelays().stream().map(StudentDelayDTO::getStd).toList();
+
+        List<String> filteredDelayedSTDs = studentService
+                .filterExistingStudentByGroup(delayedSTDs, group);
+
+        List<StudentDelayDTO> delays = missing.getStudentDelays()
+                .stream()
+                .filter(d -> filteredDelayedSTDs.contains(d.getStd()))
+                .toList();
+        missing.setStudentDelays(delays);
+
         // list of all student STDs which were present at the course
         List<String> presentSTDs = studentService.filterPresentStdsByGroup(allMissingSTDs, group);
 
-        // list of all attendances (missing & presence)  of all stds
-        List<AttendanceMatcher> attendanceMatchers = addStudentsAttendances(presentSTDs, missing, teacherRef);
+        // list of all attendances (missing & presence & delays)  of all stds
+        List<AttendanceMatcher> attendanceMatchers = addStudentsAttendances(presentSTDs, missing);
         List<MissingMatcher> missingMatchers = missingService
                 .completeMissing(missing);
 
-        return completeAttendances(attendanceMatchers, missingMatchers);
+        List<DelayMatcher> delayMatchers = delayService.completeAddDelay(delays, missing);
+
+        //result
+        return completeAttendances(attendanceMatchers, missingMatchers, delayMatchers);
     }
 
     public List<GenericAttendanceMatcher<?>> completeAttendances(
             List<AttendanceMatcher> attendanceMatchers,
-            List<MissingMatcher> missingMatchers) {
+            List<MissingMatcher> missingMatchers,
+            List<DelayMatcher> delayMatchers
+            ) {
 
         List<GenericAttendanceMatcher<?>> result = new ArrayList<>();
 
         result.addAll(attendanceMatchers);
         result.addAll(missingMatchers);
+        result.addAll(delayMatchers);
 
         return result;
     }
 
     public List<AttendanceMatcher> addStudentsAttendances(
             List<String> presentSTDs,
-            GeneralMissingDTO missing,
-            String teacherRef
+            GeneralAttendanceDTO missing
     ) {
-        Teacher responsible = teacherService.findTeacherById(teacherRef);
+        Teacher responsible = teacherService.findTeacherById(missing.getResponsibleRef().toUpperCase());
         Course course = courseService.getCourseByName(missing.getCourseName());
         List<AttendanceMatcher> attendances = new ArrayList<>();
 
@@ -183,4 +219,5 @@ public class AttendanceServiceImpl implements AttendanceService {
         }
         return attendances;
     }
+
 }
