5ceedb3f6c49e0d0bd19d221f370886b6c8607a2
Abega1642 <hei.abega.2@gmail.com>
2024-09-22 20:22:03 +0300

    chore: remove conditions in Student constructors

--- src/main/java/dev/razafindratelo/trackmyclass/entity/users/Student.java
+++ src/main/java/dev/razafindratelo/trackmyclass/entity/users/Student.java
@@ -2,17 +2,16 @@ package dev.razafindratelo.trackmyclass.entity.users;
 
 import dev.razafindratelo.trackmyclass.entity.users.enums.Group;
 import dev.razafindratelo.trackmyclass.entity.users.enums.Level;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
 @ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class Student extends User {
     private Level level;
     private Group group;
@@ -27,31 +26,7 @@ public class Student extends User {
             Group group
     ) {
         super(userRef, lastName, firstName, email, phoneNumber);
-
-        if (!doesLevelMatchesToGroup(level, group)) {
-            if (level == null && group == null) {
-                this.level = Level.L1;
-                this.group = Group.J1;
-            } else {
-                throw new IllegalArgumentException("Level doesn't match to group");
-            }
-        } else {
-            this.level = level;
-            this.group = group;
-        }
-    }
-
-    private static boolean doesLevelMatchesToGroup(Level level, Group group) {
-        return levelsGroup(level).contains(group);
-    }
-
-    private static List<Group> levelsGroup(Level level) {
-        List<Group> groups = new ArrayList<>();
-        for(Group group : Group.values()) {
-            if(group.getLevel().equals(level)) {
-                groups.add(group);
-            }
-        }
-        return groups;
+        this.level = level;
+        this.group = group;
     }
 }
