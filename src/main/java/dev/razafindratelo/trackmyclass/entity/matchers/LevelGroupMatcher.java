b9444ea5ae7ee6d6203ea9ffa4275d9ac62056cd
Abega1642 <hei.abega.2@gmail.com>
2024-09-22 22:10:51 +0300

    feat: implement LevelGroupMatcher in matchers package

--- src/main/java/dev/razafindratelo/trackmyclass/entity/matchers/LevelGroupMatcher.java
+++ src/main/java/dev/razafindratelo/trackmyclass/entity/matchers/LevelGroupMatcher.java
@@ -1,4 +1,17 @@
 package dev.razafindratelo.trackmyclass.entity.matchers;
 
+import dev.razafindratelo.trackmyclass.entity.users.enums.Group;
+import dev.razafindratelo.trackmyclass.entity.users.enums.Level;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class LevelGroupMatcher {
+    private Level level;
+    private Group group;
 }
