8c37438451f8e226fa692d6d7f9b26bf5a29dc73
Abega1642 <hei.abega.2@gmail.com>
2024-09-22 17:43:46 +0300

    feat: implement addDelay in delayServices

--- src/main/java/dev/razafindratelo/trackmyclass/services/delayServices/DelayService.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/delayServices/DelayService.java
@@ -1,6 +1,10 @@
 package dev.razafindratelo.trackmyclass.services.delayServices;
 
 
+import dev.razafindratelo.trackmyclass.dto.DelayDTO;
+import dev.razafindratelo.trackmyclass.dto.GeneralAttendanceDTO;
+import dev.razafindratelo.trackmyclass.dto.GeneralMissingDTO;
+import dev.razafindratelo.trackmyclass.dto.StudentDelayDTO;
 import dev.razafindratelo.trackmyclass.entity.matchers.DelayMatcher;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -10,4 +14,10 @@ public interface DelayService {
     List<DelayMatcher> findAllDelays();
 
     DelayMatcher findDelaysByStudentRef(String studentRef);
+
+    List<DelayMatcher> completeAddDelay(
+            List<StudentDelayDTO> lateStudentSTDs,
+            GeneralAttendanceDTO generalMissingDTO
+    );
+    List<DelayMatcher> addDelay(DelayDTO delayDTO);
 }
--- src/main/java/dev/razafindratelo/trackmyclass/services/delayServices/DelayServiceImpl.java
+++ src/main/java/dev/razafindratelo/trackmyclass/services/delayServices/DelayServiceImpl.java
@@ -1,12 +1,22 @@
 package dev.razafindratelo.trackmyclass.services.delayServices;
 
 import dev.razafindratelo.trackmyclass.dao.DelayDAO;
-import dev.razafindratelo.trackmyclass.dao.StudentDAO;
+import dev.razafindratelo.trackmyclass.dto.DelayDTO;
+import dev.razafindratelo.trackmyclass.dto.GeneralAttendanceDTO;
+import dev.razafindratelo.trackmyclass.dto.StudentDelayDTO;
+import dev.razafindratelo.trackmyclass.entity.attendances.Delay;
+import dev.razafindratelo.trackmyclass.entity.course.Course;
 import dev.razafindratelo.trackmyclass.entity.matchers.DelayMatcher;
 import dev.razafindratelo.trackmyclass.entity.users.Student;
+import dev.razafindratelo.trackmyclass.entity.users.Teacher;
+import dev.razafindratelo.trackmyclass.services.courseServices.CourseService;
+import dev.razafindratelo.trackmyclass.services.studentServices.StudentService;
+import dev.razafindratelo.trackmyclass.services.teacherServices.TeacherService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,23 +25,100 @@ import java.util.List;
 @AllArgsConstructor
 public class DelayServiceImpl implements DelayService {
     private final DelayDAO delayDAO;
-    private final StudentDAO studentDAO;
+    private final StudentService studentService;
+    private final CourseService courseService;
+    private final TeacherService teacherService;
 
     @Override
     public List<DelayMatcher> findAllDelays() {
-        List<String> stds = studentDAO.getAllStudentRef();
+        List<String> stds = studentService.findAllStudentRef();
         List<DelayMatcher> delays = new ArrayList<>();
 
         for (String std : stds) {
-            Student student = studentDAO.getStudentById(std);
-            delays.add(delayDAO.getDelaysByStudent(student));
+            Student student = studentService.findStudentById(std);
+            delays.add(findDelaysByStudentRef(std));
         }
         return delays;
     }
 
     @Override
     public DelayMatcher findDelaysByStudentRef(String studentRef) {
-        Student student = studentDAO.getStudentById(studentRef);
+        Student student = studentService.findStudentById(studentRef);
         return delayDAO.getDelaysByStudent(student);
+    }
+
+    @Override
+    public List<DelayMatcher> completeAddDelay(List<StudentDelayDTO> lateStudentSTDs, GeneralAttendanceDTO generalAttendance) {
+        String levelYear = courseService.getCourseLevelYear(generalAttendance.getCourseName()).toString();
+
+        lateStudentSTDs = lateStudentSTDs
+                .stream()
+                .filter(std -> studentService.checkIfStudentExistsByLevelYear(levelYear, std.getStd())).toList();
+
+        Teacher responsible = teacherService.findTeacherById(generalAttendance.getResponsibleRef());
+        Course course = courseService.getCourseByName(generalAttendance.getCourseName());
+
+        return constructDelayMatchers(
+                generalAttendance.getCommencement(),
+                generalAttendance.getTermination(),
+                lateStudentSTDs,
+                course,
+                responsible
+        );
+    }
+
+    @Override
+    public List<DelayMatcher> addDelay(DelayDTO delayDTO) {
+
+        List<String> STDs = delayDTO.getDelays().
+                stream()
+                .map(StudentDelayDTO::getStd)
+                .toList();
+
+        String levelYear = courseService.getCourseLevelYear(delayDTO.getCourseName()).toString();
+        List<String> filteredSTDs = studentService.filterExistingStudentByLevelYear(STDs, levelYear);
+
+        List<StudentDelayDTO> delays = delayDTO.getDelays()
+                .stream()
+                .filter(delay -> filteredSTDs.contains(delay.getStd()))
+                .toList();
+
+        Course course = courseService.getCourseByName(delayDTO.getCourseName());
+
+        Teacher responsible = teacherService.findTeacherById(delayDTO.getResponsibleRef());
+
+
+        return constructDelayMatchers(
+                delayDTO.getCommencement(),
+                delayDTO.getTermination(),
+                delays,
+                course,
+                responsible
+        );
+    }
+
+    public List<DelayMatcher> constructDelayMatchers(
+            LocalDateTime commencement,
+            LocalDateTime termination,
+            List<StudentDelayDTO> delays,
+            Course course,
+            Teacher responsible
+    ) {
+        List<DelayMatcher> result = new ArrayList<>();
+
+        for(StudentDelayDTO del : delays) {
+            Student student = studentService.findStudentById(del.getStd());
+
+            Delay delay = delayDAO.addDelay(
+                    del.getStd(),
+                    course,
+                    responsible,
+                    commencement,
+                    termination,
+                    del.getLateness()
+            );
+            result.add(new DelayMatcher(student, List.of(delay)));
+        }
+        return result;
     }
 }
