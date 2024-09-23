4c014407d31502c71686739d8acb888a54c04e83
Abega1642 <hei.abega.2@gmail.com>
2024-09-23 20:57:00 +0300

    feat: implement PUT and DELETE endpoint + api specification

--- src/main/java/dev/razafindratelo/trackmyclass/controllers/MissingController.java
+++ src/main/java/dev/razafindratelo/trackmyclass/controllers/MissingController.java
@@ -1,13 +1,15 @@
 package dev.razafindratelo.trackmyclass.controllers;
 
 import dev.razafindratelo.trackmyclass.dto.GeneralMissingDTO;
+import dev.razafindratelo.trackmyclass.dto.MissingDTO;
+import dev.razafindratelo.trackmyclass.entity.attendances.Missing;
 import dev.razafindratelo.trackmyclass.entity.matchers.MissingMatcher;
 import dev.razafindratelo.trackmyclass.services.missingServices.MissingService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -47,5 +49,41 @@ public class MissingController {
                 missingService.completeMissing(missing),
                 HttpStatus.CREATED
         );
+    }
+
+    @PutMapping("student/{std}/missing/update")
+    public ResponseEntity<MissingMatcher> updateStudentMissingByStudent(
+            @PathVariable("std") String std,
+            @RequestParam("course_name") String courseName,
+            @RequestParam("course_started_at") LocalDateTime courseStartedAt,
+            @RequestParam("course_ended_at") LocalDateTime courseEndedAt,
+            @RequestBody MissingDTO missing
+    ) {
+        return new ResponseEntity<>(
+                missingService.updateMissing(
+                    courseName,
+                    courseStartedAt,
+                    courseEndedAt,
+                    missing
+                ),
+                HttpStatus.OK
+        );
+    }
+
+    @DeleteMapping("/student/{std}/missing/delete")
+    public ResponseEntity<Missing> deleteMissing(
+            @PathVariable("std") String std,
+            @RequestParam("course_name") String courseName,
+            @RequestParam("course_started_at") LocalDateTime courseStartedAt,
+            @RequestParam("course_ended_at") LocalDateTime courseEndedAt,
+            @RequestParam("responsibleRef") String responsibleRef
+    ) {
+        return ResponseEntity.ok(missingService.deleteMissing(
+                courseName,
+                std,
+                courseStartedAt,
+                courseEndedAt,
+                responsibleRef
+        ));
     }
 }
