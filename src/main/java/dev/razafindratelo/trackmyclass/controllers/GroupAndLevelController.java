b878e9205b157843df048162633f2a70195f8ad7
Abega1642 <hei.abega.2@gmail.com>
2024-09-23 07:20:38 +0300

    chore: remove upgrade endpoint from GroupAndLevelController

--- src/main/java/dev/razafindratelo/trackmyclass/controllers/GroupAndLevelController.java
+++ src/main/java/dev/razafindratelo/trackmyclass/controllers/GroupAndLevelController.java
@@ -1,16 +1,12 @@
 package dev.razafindratelo.trackmyclass.controllers;
 
 import dev.razafindratelo.trackmyclass.entity.matchers.LevelGroupMatcher;
-import dev.razafindratelo.trackmyclass.entity.users.Student;
 import dev.razafindratelo.trackmyclass.entity.users.enums.Group;
 import dev.razafindratelo.trackmyclass.entity.users.enums.Level;
 import dev.razafindratelo.trackmyclass.services.groupAndLevelServices.GroupAndLevelService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -34,11 +30,4 @@ public class GroupAndLevelController {
         return ResponseEntity.ok(groupAndLevelService.getAllLevels());
     }
 
-    @PutMapping("/student/upgrade")
-    public ResponseEntity<List<Student>> upgrade(List<String> excludedSTDs) {
-        return new ResponseEntity<>(
-                groupAndLevelService.updateAllLevelGroupRelationAndStudentToNextLevelYear(excludedSTDs),
-                HttpStatus.OK
-        );
-    }
 }
